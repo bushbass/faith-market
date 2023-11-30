943566d0f4a61cae2ead4d988a802ee2f310dfc0
Alex Nielsen <alex@bigal.org>
2023-11-30 12:33:34 -0500

    added || 4000 to port

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT || 4000, () => {
       console.log('connected to db & listening on port', process.env.PORT);
     });
   })
