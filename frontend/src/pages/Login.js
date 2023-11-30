0f641ce133eb2a3d4f60bf117727732523a85613
Alex Nielsen <alex@bigal.org>
2023-11-30 11:30:30 -0500

    added name attribute to login form

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -22,12 +22,14 @@ const Login = () => {
         <label>Email address:</label>
         <input
           type="email"
+          name="email"
           onChange={(e) => setEmail(e.target.value)}
           value={email}
         />
         <label>Password:</label>
         <input
           type="password"
+          name="password"
           onChange={(e) => setPassword(e.target.value)}
           value={password}
         />
