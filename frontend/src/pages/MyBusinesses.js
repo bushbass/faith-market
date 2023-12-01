4308878309002747ebaee525d1f095c8adab7fe6
Alex Nielsen <alex@bigal.org>
2023-12-01 12:00:09 -0500

    trying not auth on me again

---
+++ frontend/src/pages/MyBusinesses.js
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import { useBusinessesContext } from '../hooks/useBusinessesContext';
+import { useAuthContext } from '../hooks/useAuthContext';
+
+// components
+import BusinessDetails from '../components/BusinessDetails';
+import BusinessForm from '../components/BusinessForm';
+
+const MyBusinesses = () => {
+  const { myBusinesses, dispatch } = useBusinessesContext();
+  const { user } = useAuthContext();
+
+  useEffect(() => {
+    const fetchMyBusinesses = async () => {
+      const response = await fetch('/api/businesses/me', {
+        headers: { Authorization: `Bearer ${user.token}` },
+      });
+      const json = await response.json();
+
+      console.log(json);
+      if (response.ok) {
+        dispatch({ type: 'SET_MY_BUSINESSES', payload: json });
+      }
+    };
+
+    if (user) {
+      fetchMyBusinesses();
+    }
+  }, [dispatch, user]);
+
+  return (
+    <div className="home">
+      <div className="businesses">
+        <h2>My Businesses</h2>
+        {myBusinesses &&
+          myBusinesses.map((business) => (
+            <BusinessDetails key={business._id} business={business} />
+          ))}
+      </div>
+      <BusinessForm />
+    </div>
+  );
+};
+
+export default MyBusinesses;
